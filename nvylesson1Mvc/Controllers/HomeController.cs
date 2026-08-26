c092f5a0886c6bbb0180fa745e09d0bb9bcc9258
yen2112 <yendzvl2006@gmail.com>
2026-08-26 17:12:05 +0700

    nvylesson01

---
+++ nvylesson1Mvc/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using nvylesson1Mvc.Models;
+using System.Diagnostics;
+
+namespace nvylesson1Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.name = "NGUYEN VAN YEN";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ nvylesson1Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace nvylesson1Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
